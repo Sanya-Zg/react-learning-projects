9e2b2d3065735388f4ccdd1e885fa835646e41c5
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-06-16 21:50:04 +0300

    create rating component

--- react-learning-projects/src/App.jsx
+++ react-learning-projects/src/App.jsx
@@ -2,15 +2,16 @@
 import Accordion from './components/accordion/Accordion'
 import List_checkbox from './components/list-checkboxes/List_checkbox'
 import RandomColor from './components/random-color/RandomColor'
-
+import StarRating from './components/star-rating/StarRating'
 
 function App() {
 
   return (
     <>
-      <Accordion />
+      {/* <Accordion />
       <List_checkbox />
-      <RandomColor />
+      <RandomColor /> */}
+      <StarRating numStars={10}/>
     </>
   )
 }
---
+++ react-learning-projects/src/components/star-rating/StarRating.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import styles from "./Rating.module.css";
+import { FaStar } from "react-icons/fa";
+
+const StarRating = ({ numStars = 5 }) => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(currIndex) {
+    setRating(currIndex)
+  }
+  function handleMouseEnter(currIndex) {
+    setHover(currIndex)
+  }
+  function handleMouseLeave() {
+    setHover(rating)
+  }
+  return (
+    <div className={styles.cont}>
+      {[...Array(numStars)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? styles.active : styles.unactive}
+            onClick={() => handleClick(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default StarRating;
